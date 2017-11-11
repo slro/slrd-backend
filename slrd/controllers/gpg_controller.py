e8bf93ff3b319d56fdc1141cb2eb645076ff7b97
ddnomad <6703766+ddnomad@users.noreply.github.com>
2017-11-11 16:37:46 +0200

    fixed docstrings in gpg_controller.py

--- slrd/controllers/gpg_controller.py
+++ slrd/controllers/gpg_controller.py
@@ -1,54 +1,64 @@
+"""This module contains a single class: GPGController.
+
+See GPGController class documentation for additional details.
+"""
+import gnupg
+
+
 class GPGController(object):
-    """ Encrypt and decrypt data with GnuPG """
+    """Encrypt and decrypt data with GnuPG."""
 
     def __init__(self, gpg_key_id, pt_length):
-        """ Initialization method
+        """Initialization method.
 
-        :param str gpg_key_id: ID of GPG key to use for encryption/decryption
-        :param int pt_length:  length of a plain text block to feed to GPG (it
-                               will be padded to this length and rejected if
-                               it's longer)
+        :param gpg_key_id: ID of GPG key to use for encryption/decryption
+        :param pt_length: length of a plain text block to feed to GPG (it will
+                          be padded to this length and rejected if it's longer)
+        :type gpg_key_id: str
+        :type pt_length: int
 
         :raise: <???>
-
         """
-
-        # self.gpg_key_id = gpg_key_id
+        self.gpg_key_id = gpg_key_id
+        self.gpg = gnupg.GPG(gnupghome='???')
 
     def pad(self, data):
-        """ Pad data to self.pt_length
+        """Pad data to the length equal to len(self.pt_length).
 
-        :param str data: data to pad
+        :param data: data to pad
+        :type data: str
 
-        :return str: padded data
-
+        :return: padded data
+        :rtype: str
         """
 
     def encrypt(self, data):
-        """ Encrypt data with GPG
+        """Encrypt data with GPG.
 
-        :param str data: data to encrypt
+        :param data: data to encrypt
+        :type data: str
 
-        :return <???>: encrypted data # TODO: what type?
+        :return: encrypted data
+        :rtype: <???> # TODO: what type?
 
         :raise: <???>
-
         """
 
-        # make sure len(data) < self.pt_length
+        # Make sure len(data) < self.pt_length
 
-        # pad data first
+        # Pad data first
         # self.pad(data)
 
     def decrypt(self, data):
-        """ Decrypt data with PGP
+        """Decrypt data with PGP.
 
-        :param str data: data to decrypt
+        :param data: data to decrypt
+        :type data: str
 
-        :return <???>: decrypted data # TODO: what type?
+        :return: decrypted data
+        :rtype: <???> # TODO: what type?
 
         :raise: <???>
-
         """
 
-        # make sure len(data) == len(pt_length) + GPG overhead
+        # Make sure len(data) == len(pt_length) + GPG overhead
