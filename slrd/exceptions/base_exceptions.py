9edb9dabade55db9ad72bcb3dbb941dbf5819722
ddnomad <6703766+ddnomad@users.noreply.github.com>
2017-12-02 20:40:55 +0200

    defined types of base exceptions

--- slrd/exceptions/base_exceptions.py
+++ slrd/exceptions/base_exceptions.py
@@ -7,14 +7,14 @@ class SLRDException indicates that the exception is related to SLRD program and
 was a result of program-specific causes.
 
 SLRDRuntimeException is a base class for exceptions that happen due to a
-dynimic input from the user or is a result of external factors. This type of
-exceptions should be caught and handled properly to provide a verbose
-explanation of the causes.
+dynimic input from a user or is a result of certain external factors. This type
+of exceptions should be caught and handled properly to provide a verbose
+explanation of the causes (bubble up to user interface etc).
 
 SLRDImplementationError is a base class for exceptions that are a result of an
 improper usage of SLRD internal modules (controllers etc.). This type of
 exceptions should not happen at all thus should not be caught or processed in
-any way except for ensuring graceful shutdown in an extreme occasions.
+any way except for ensuring graceful shutdown in extreme occasions.
 
 Classes:
     - SLRDException
@@ -26,7 +26,7 @@ Classes:
 class SLRDException(Exception):
     """Base exceptions class.
 
-    Passed an already formatted exception message to its parent class to be
+    Pass an already formatted exception message to its parent class to be
     printed out to STDOUT.
     """
 
@@ -37,3 +37,16 @@ class SLRDException(Exception):
         :type msg:  str
         """
         super(type(self), self).__init__(msg)
+
+
+class SLRDRuntimeException(SLRDException):
+    """Base external input related exception class."""
+
+
+class SLRDImplementationError(SLRDException):
+    """Base internal error related exception class.
+
+    In other words the error is critical and is a result of improper usage of
+    internal modules. Should not be caught and generally should not happen at
+    all. Think of it as a some kind of assertion.
+    """
