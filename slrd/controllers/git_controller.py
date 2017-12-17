621a1febee928c95152d6424c3a28119c032e4d0
vsmysle <kyrylo.voronchenko@gmail.com>
2017-12-17 23:13:19 +0200

    added logging for each method except rollback

--- slrd/controllers/git_controller.py
+++ slrd/controllers/git_controller.py
@@ -24,15 +24,23 @@ class GitController(object):
         self.logger = logging.getLogger(__name__)
         self.logger.debug(comlogstr.LOG_INIT_START)
         self.init = init
-        self.remote_url = None
+        self.origin = None
         if self.init:
-            self.logger.info(gitlogstr.LOGSTR_SET_REPO % path)
-            self.repo = Repo(path)
-            self.logger.debug(gitlogstr.LOGSTR_SET_REPO_SUCCESSFUL % path)
+            try:
+                self.logger.info(gitlogstr.LOGSTR_SET_REPO_STARTED % path)
+                self.repo = Repo(path)
+                self.logger.debug(gitlogstr.LOGSTR_SET_REPO_SUCCESS % path)
+            except:
+                self.logger.error(gitlogstr.LOGSTR_SET_REPO_ERROR)
+            self.logger.info(gitlogstr.LOGSTR_SET_REPO_ENDED)
         else:
-            self.logger.info(gitlogstr.LOGSTR_INIT_BARE_REPO_STARTED % path)
-            self.repo = Repo.init(path, 'bare-repo', bare=True)
-            self.logger.debug(gitlogstr.LOGSTR_INIT_BARE_REPO_ENDED % path)          
+            try:
+                self.logger.info(gitlogstr.LOGSTR_INIT_STARTED % path)
+                self.repo = Repo.init(path, 'bare-repo', bare=True)
+                self.logger.debug(gitlogstr.LOGSTR_INIT_ENDED % path)          
+            except:
+                self.logger.error(gitlogstr.LOGSTR_INIT_ERROR)
+            self.logger.info(gitlogstr.LOGSTR_INIT_ENDED)
         self.logger.debug(comlogstr.LOG_INIT_END)
 
     def commit(self, commit_message):
@@ -42,12 +50,14 @@ class GitController(object):
         :param commit_message: commit message
         :type  commit_message: str
         """
-        # git add started( TODO: logging )
-        self.repo.index.add(self.repo.untracked_files)
-        # git add ended ( TODO: logging)
-        # git commit started ( TODO: logging)
-        self.repo.commit(commit_message)
-        # git commit ended (TODO: logging)
+        # Do we need to add files here? Or this is done by manager?
+        try:
+            self.logger.info(gitlogstr.LOGSTR_COMMIT_STARTED)
+            self.repo.commit(commit_message)
+            self.logger.info(gitlogstr.LOGSTR_COMMIT_SUCCESS)
+        except:
+            self.logger.error(gitlogstr.LOGSTR_ADD_ERROR)
+        self.logger.info(gitlogstr.LOGSTR_ADD_ENDED)
 
     def set_remote(self, url):
         """
@@ -56,19 +66,64 @@ class GitController(object):
         :param url: url to a remote repository
         :type url:  str
         """
-        self.remote_url = url
-        # git remote add origin 'url' (TODO: logging)
-        self.repo.create_remote('origin', url)
-        # git remote adding was successful (TODO: logging)
+        try:
+            self.logger.info(gitlogstr.LOGSTR_SET_REMOTE_STARTED)
+            self.origin = self.repo.create_remote('origin', url)
+            self.logger.info(gitlogstr.LOGSTR_SET_REMOTE_SUCCESS)
+        except:
+            self.logger.error(gitlogstr.LOGSTR_SET_REMOTE_ERROR)
+        self.logger.info(gitlogstr.LOGSTR_SET_REMOTE_ENDED)
 
     def pull(self):
         """
         Pull from git remote repository.
         """
-        pass
+        if self.origin:
+            try:
+                self.logger.info(gitlogstr.LOGSTR_PULL_STARTED)
+                self.origin.pull()
+                self.logger.info(gitlogstr.LOGSTR_PULL_SUCCESS)
+            except:
+                self.logger.error(gitlogstr.LOGSTR_PULL_ERROR)
+            self.logger.info(gitlogstr.LOGSTR_PULL_ENDED)
+        else:
+            self.logger.error(gitlogstr.LOGSTR_REMOTE_NOT_SET)
+
+    def push(self):
+        """
+        Push to git remote repository.
+        """
+        if self.origin:
+            try:
+                self.logger.info(gitlogstr.LOGSTR_PUSH_STARTED)
+                self.origin.push()
+                self.logger.info(gitlogstr.LOGSTR_PUSH_SUCCESS)
+            except:
+                self.logger.error(gitlogstr.LOGSTR_PUSH_ERROR)
+            self.logger.info(gitlogstr.LOGSTR_PUSH_ENDED)
+        else:
+            self.logger.error(gitlogstr.LOGSTR_REMOTE_NOT_SET)
 
     def rollback(self):
         """
         Rollbase to a previous commit.
         """
         pass
+
+    def __add(self, files=None):
+        """
+        Git add.
+
+        :param files: list of paths of the files to add
+        :type files: list
+        """
+        if not files:
+            try:
+                self.logger.info(gitlogstr.LOGSTR_ADD_STARTED)
+                self.repo.index.add(self.repo.untracked_files)
+                self.logger.info(gitlogstr.LOGSTR_ADD_SUCCESS)
+            except:
+                self.logger.error(gitlogstr.LOGSTR_ADD_ERROR)
+            self.logger.info(gitlogstr.LOGSTR_ADD_ENDED)
+        else:
+            self.repo.index.add(files)
--- slrd/strings/git_controller_logstrings.py
+++ slrd/strings/git_controller_logstrings.py
@@ -6,7 +6,40 @@ class GitControllerLogstrings(object):
     """
     Logstring of the GitController.
     """
-    LOGSTR_INIT_BARE_REPO_STARTED = "initialization base git repo in %s started"
-    LOGSTR_INIT_BARE_REPO_ENDED = "initialization base git repo in %S ended"
-    LOGSTR_SET_REPO = "trying to set git repo in %s ..."
-    LOGSTR_SET_REPO_SUCCESSFUL = "set git repo in %s was successful!"
+    # Git init logstrings
+    LOGSTR_INIT_STARTED = "initialization base git repo in %s started"
+    LOGSTR_INIT_ENDED = "initialization base git repo in %S ended"
+    LOGSTR_INIT_ERROR = ""
+    LOGSTR_INIT_SUCCESS = ""
+    # Use exisiting git repository
+    LOGSTR_SET_REPO_STARTED = "trying to set git repo in %s ..."
+    LOGSTR_SET_REPO_ENDED = ""
+    LOGSTR_SET_REPO_ERROR = ""
+    LOGSTR_SET_REPO_SUCCESS = "set git repo in %s was successful!"
+    # Git add logstrings
+    LOGSTR_ADD_STARTED = ""
+    LOGSTR_ADD_ENDED = ""
+    LOGSTR_ADD_ERROR = ""
+    LOGSTR_ADD_SUCCESS = ""
+    # Git pull logstrings
+    LOGSTR_PULL_STARTED = ""
+    LOGSTR_PULL_ENDED = ""
+    LOGSTR_PULL_ERROR = ""
+    LOGSTR_PULL_SUCCESS = ""
+    # Git push logstrings
+    LOGSTR_PUSH_STARTED = ""
+    LOGSTR_PUSH_ENDED = ""
+    LOGSTR_PUSH_ERROR = ""
+    LOGSTR_PUSH_SUCCESS = ""
+    # Git commit logstrings
+    LOGSTR_COMMIT_STARTED = ""
+    LOGSTR_COMMIT_ENDED = ""
+    LOGSTR_COMMIT_ERROR = ""
+    LOGSTR_COMMIT_SUCCESS = ""
+    # Git remote logstrings
+    LOGSTR_SET_REMOTE_STARTED = ""
+    LOGSTR_SET_REMOTE_ENDED = ""
+    LOGSTR_SET_REMOTE_ERROR = ""
+    LOGSTR_SET_REMOTE_SUCCESS = ""
+    # Git remote not set
+    LOGSTR_REMOTE_NOT_SET = ""
